50cbdbfb4ba2d64b50fe2c47210af9052570e953
Lei Peng <101799503+penglei0@users.noreply.github.com>
2023-09-16 02:31:09 +0000

    chore: add crtp benchmark results

--- crtp_benchmark.cc
+++ crtp_benchmark.cc
@@ -71,3 +71,22 @@ BENCHMARK(BM_virtual_function);
 BENCHMARK(BM_crtp_pattern);
 
 BENCHMARK_MAIN();
+
+/* Benchmark result:
+```text
+2023-09-16T02:30:24+00:00
+Running ./crtp_benchmark
+Run on (2 X 3176.12 MHz CPU s)
+CPU Caches:
+  L1 Data 32 KiB (x1)
+  L1 Instruction 32 KiB (x1)
+  L2 Unified 512 KiB (x1)
+  L3 Unified 32768 KiB (x1)
+Load Average: 0.30, 0.25, 0.25
+--------------------------------------------------------------
+Benchmark                    Time             CPU   Iterations
+--------------------------------------------------------------
+BM_virtual_function       1.12 ns         1.11 ns    652243999
+BM_crtp_pattern          0.313 ns        0.313 ns   1000000000
+```
+*/
